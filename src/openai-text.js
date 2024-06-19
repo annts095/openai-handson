61f186512452e384f106f2003bbfab71217db236
annts095 <anantasuku@team-lab.com>
2024-06-19 21:50:18 +0900

    feat: テキストを引数から渡せるようにしつつコマンド修正

--- src/openai-text.js
+++ src/openai-text.js
@@ -3,8 +3,11 @@ import OpenAI from "openai";
 const openai = new OpenAI();
 
 async function main() {
+  // コマンド実行時に渡されたテキストを取得
+  const text = process.argv[2];
+
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "面白いアプリの作り方" }],
+    messages: [{ role: "system", content: text }],
     model: "gpt-3.5-turbo",
   });
 
